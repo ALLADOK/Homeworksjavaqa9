9614632c0f167e05a1eead199a102f4db25fbca5
ALLA <all.tarasowa808@yandex.ru>
2024-05-12 13:25:17 +0300

    Reformat2

--- src/test/java/ru/netology/statistic/StatisticsService/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsService/StatisticsServiceTest.java
@@ -16,7 +16,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMax1(){
+    void findMax1() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
